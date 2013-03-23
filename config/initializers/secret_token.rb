f0216168bb218a2be71662eeacf711e3d22c2050
David Vezzani <david_vezzani@yahoo.com>
2013-03-23 10:31:54 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+My401kCms::Application.config.secret_token = '215ae506c7f0364c90f668d9bf887cd5886c37f110d8fca4611c1c607c2ed45ee6f2b3d7f828286f85840a5f4242b713f1f2c4863ab9f881cd5423ec73a094a1'
